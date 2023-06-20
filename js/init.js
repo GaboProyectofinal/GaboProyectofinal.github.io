e38b10b5c1ddc77efb7ed8ce43831a8147993b78
GaboProyectofinal <137135352+GaboProyectofinal@users.noreply.github.com>
2023-06-20 13:11:20 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -9,11 +9,20 @@
 // @ts-ignore
 
 const firebaseConfig = {
+
   apiKey: "AIzaSyCOIF-_v-Li3joTbdOJfvL1SwaaO71nCzA",
+
   authDomain: "proyectofinalbd-28a57.firebaseapp.com",
+
   projectId: "proyectofinalbd-28a57",
+
   storageBucket: "proyectofinalbd-28a57.appspot.com",
+
   messagingSenderId: "644950133334",
+
   appId: "1:644950133334:web:f71b8ffb4d269502ec89f7",
+
   measurementId: "G-G6202V51B2"
+
 };
+
